846b6601840fd7e0cfb22b10d9a08aa84c109794
Alex <alex@gu3.me>
2021-05-12 01:41:25 +0100

    Update NOTES Clean up HeroPortrait Add portrait property to hero json files

--- code/UI/Bottom/HeroPortrait.cs
+++ code/UI/Bottom/HeroPortrait.cs
@@ -8,9 +8,8 @@ namespace Extraction.UI
 	// TODO
 	public class HeroPortrait : Panel
 	{
-		SceneCapture sceneCapture;
-		Angles CamAngles;
 		public Label heroText;
+		private Image heroImage;
 
 		public HeroPortrait()
 		{
@@ -19,22 +18,6 @@ namespace Extraction.UI
 			Style.AlignItems = Align.Center;
 			Style.AlignContent = Align.Center;
 			StyleSheet.Parse( "image { width: 128px; height: 128px; justify-content: center; align-items: center; }" );
-
-			// CamAngles = new Angles( 25, 0.0f, 0 );
-			//
-			// using ( SceneWorld.SetCurrent( new SceneWorld() ) )
-			// {
-			// 	var playerPrevObj = SceneObject.CreateModel( "models/citizen/citizen.vmdl", Transform.Zero );
-			// 	playerPrevObj.Transform = new Transform( default, Rotation.From(new Angles(0, 180, 0  )) );
-			//
-			// 	sceneCapture = SceneCapture.Create( "test", 512, 512 );
-			// 	sceneCapture.ClearColor = Color.Transparent;
-			// 	sceneCapture.AmbientColor = Color.White;
-			//
-			// 	sceneCapture.SetCamera( Vector3.Up * 50 + CamAngles.Direction * -50, CamAngles, 45 );
-			// }
-			//
-			// Add.Image( "scene:test" );
 
 			Add.Image( "ui/extraction/hero-portrait-placeholder.png", "hero-image" );
 			
@@ -52,12 +35,10 @@ namespace Extraction.UI
 			heroText.Text = $"{player.HeroData.Name}";
 		}
 
-		public override void OnDeleted()
+		[Event( "hero_change" )] // TODO
+		public void UpdateHeroPortrait()
 		{
-			base.OnDeleted();
-
-			sceneCapture?.Delete();
-			sceneCapture = null;
+			heroImage = Add.Image( "ui/extraction/hero-portrait-placeholder.png", "hero-image" );
 		}
 	}
 }
