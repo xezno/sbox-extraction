31b91f4f8c33a6639f216d47b05299b64259bbe9
Alex <alex@gu3.me>
2021-05-15 00:08:54 +0100

    Fix shotgun reloads

--- code/Weapons/Shotgun.cs
+++ code/Weapons/Shotgun.cs
@@ -20,11 +20,51 @@ namespace Extraction.Weapons
 
 		public override float PrimaryRate => 1.5f;
 		public override float SecondaryRate => 1.0f;
-		public override float ReloadTime => 1.0f;
+		public override float ReloadTime => 0.7f;
 
-		public override bool AutoFire => false;
+		public override bool AutoFire => true;
 		public override HoldType WeaponHoldType => HoldType.Rifle;
 
 		public override int Slot => 0;
+
+		public override void OnReloadFinish()
+		{
+			IsReloading = false;
+			TimeSincePrimaryAttack = 0;
+			
+			void Finished() => ViewModelEntity?.SetAnimParam( "reload_finished", true ); 
+
+			if ( AmmoClip >= ClipSize )
+			{
+				Finished();
+				return;
+			}
+
+			if ( AvailableAmmo() != 0 )
+			{
+				AmmoClip++;
+				ReserveAmmo--;
+			}
+			else
+			{
+				Finished();
+				return;
+			}
+
+			if ( Owner.Input.Down( InputButton.Attack1 ) ) // Janky way to pause reloads if we're between shells
+			{
+				Finished();
+				return;
+			}
+
+			if ( AmmoClip < ClipSize && AvailableAmmo() > 0 )
+			{
+				Reload();
+			}
+			else
+			{
+				Finished();
+			}
+		}
 	}
 }
